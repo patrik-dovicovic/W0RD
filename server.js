3c8f67185afd466cd681f3376d241ac7e102e54f
patrik_dovicovic <72455555+patrik-dovicovic@users.noreply.github.com>
2022-02-13 23:22:32 +0100

    a

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
+const PORT = process.env.PORT || 5000;
 
 //var
 var word = "slnko"
@@ -23,7 +24,7 @@ fs.readFile('sk.txt', function (err, read_data) {
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json());
-app.listen(80);
+app.listen(PORT);
 console.log("ok")
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/', 'new.html'));
